07c546616eb4c234a59ce284e134259339867e2e
FiveWing96 <117134067+FiveWing96@users.noreply.github.com>
2023-02-07 15:28:17 +0700

    first data

---
+++ src/practice/FindMaxValue.java
@@ -0,0 +1,24 @@
+package practice;
+
+import file.txt.ReadAndFileWrite;
+
+import java.util.List;
+
+public class FindMaxValue {
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (max < numbers.get(i)) {
+                max = numbers.get(i);
+            }
+        }
+        return max;
+    }
+    public static  void main(String[] args) {
+
+        ReadAndFileWrite readAndWriteFile = new ReadAndFileWrite();
+        List<Integer> numbers = readAndWriteFile.readFile("numbers.txt");
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile("result.txt", maxValue);
+    }
+}
